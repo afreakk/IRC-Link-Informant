43d2e031d1b5502ee76c1da6f69a90d6a2776b22
hans <laderud@hotmail.com>
2020-02-25 19:11:06 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,16 @@ use std::net::TcpStream;
 use select::document::Document;
 use select::predicate::Name;
 
-fn get_title_from_link(link: &linkify::Link) -> Result<String, String> {
+fn get_title_from_link(link: linkify::Link) -> Result<String, String> {
     reqwest::blocking::get(link.as_str())
         .map_err(|e| e.to_string())
         .and_then(|res| Document::from_read(res).map_err(|e| e.to_string()))
-        .and_then(|x| {
-            x.find(Name("title"))
+        .and_then(|doc| {
+            doc.find(Name("title"))
                 .next()
                 .ok_or(format!("No title found on url {}", link.as_str()))
-                .map(|n| n.text())
+                .map(|elem| elem.text())
         })
-}
-
-fn extract_links(s: &str) -> Vec<linkify::Link> {
-    linkify::LinkFinder::new().links(s).collect()
 }
 
 fn print_and_discard(r: &Result<String, String>) {
@@ -55,52 +51,39 @@ fn main() -> Result<(), String> {
         TcpStream::connect(&s.server)
             .map_err(|e| e.to_string())
             .map(|tcp_stream| BufStream::new(tcp_stream))
-            .and_then(|mut stream| {
-                send_raw_msg_to_stream(&mut stream, &format!("NICK {}", &s.nick))
+            .and_then(|mut bufstream| {
+                send_raw_msg_to_stream(&mut bufstream, &format!("NICK {}", &s.nick))
                     .and(send_raw_msg_to_stream(
-                        &mut stream,
+                        &mut bufstream,
                         &format!("USER {} 0 * :{}", &s.user, &s.name),
                     ))
                     .and(send_raw_msg_to_stream(
-                        &mut stream,
+                        &mut bufstream,
                         &format!("JOIN {}", &s.channel),
                     ))
-                    .map(|_| irc_loop(stream, s))
+                    .map(|_| irc_loop(bufstream, s))
             })
     })
 }
 
-fn irc_loop(mut stream: BufStream<TcpStream>, s: Settings) {
+fn irc_loop(mut bufstream: BufStream<TcpStream>, s: Settings) {
     let split_by_channel = format!("PRIVMSG {}", &s.channel);
     let mut buffer = String::new();
-    while let Ok(_) = stream.read_line(&mut buffer) {
+    while let Ok(_) = bufstream.read_line(&mut buffer) {
         print!(">> {}", buffer);
         if buffer.starts_with("PING") {
             print_and_discard(&send_raw_msg_to_stream(
-                &mut stream,
+                &mut bufstream,
                 &buffer.replace("PING", "PONG").trim_end(),
             ));
         } else {
-            buffer
-                .split(&split_by_channel)
-                .nth(1)
-                .ok_or("Not a channel msg".to_owned())
-                .map(extract_links)
-                .map(|links| {
-                    links
-                        .iter()
-                        .map(get_title_from_link)
-                        .map(|r| {
-                            r.and_then(|title| {
-                                send_raw_msg_to_stream(
-                                    &mut stream,
-                                    &as_channel_msg(&s.channel, &title),
-                                )
-                            })
-                        })
-                        .for_each(|x| print_and_discard(&x))
+            buffer.split(&split_by_channel).nth(1).map(|chan_msg| {
+                linkify::LinkFinder::new().links(chan_msg).for_each(|link| {
+                    print_and_discard(&get_title_from_link(link).and_then(|title| {
+                        send_raw_msg_to_stream(&mut bufstream, &as_channel_msg(&s.channel, &title))
+                    }))
                 })
-                .unwrap_or_else(|e| println!("e: {}", e));
+            });
         }
         buffer.clear();
     }
